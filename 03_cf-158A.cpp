8ded621c2d75e921f15c5f6d7428c2fa395ed020
hasofn <hasanruzweb@gmail.com>
2022-01-04 09:39:29 +0300

    third problem

---
+++ 03_cf-158A.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int n;
+	int k;
+	cin >> n >> k;
+	int a[n];
+	for (int i = 0; i < n; i++){
+		cin >> a[i];	
+	}
+	int nextround = 0;
+	for (int i = 0; i < n; i++){
+		if(a[i] >= a[k - 1] && a[i] > 0){
+			nextround++;
+
+		}
+
+	}
+	cout << nextround;
+
+}
