ac3e1879ee7af77a63862dcdf00ce607d6fc1760
Hasan Rüzgar <hasanruzweb@gmail.com>
2022-02-13 16:18:38 +0100

    solved a pretty good problem

--- mal-guck.cpp
+++ mal-guck.cpp
@@ -2,6 +2,18 @@
 using namespace std;
 
 int main(){
-	
-	
+	int tcases;
+	cin >> tcases;
+
+	for(int i = 0; i < tcases; i++){
+		int c_health, c_attack;
+		cin >> c_health >> c_attack;
+
+		int m_health, m_attack;
+		cin >> m_health >> m_attack;
+
+		
+
+	}
+
 }
